23c7f5f2d8296cd014915842b2a73a07a4e7b8af
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-02 15:11:38 -0500

    reiniciar estado despues de la consulta

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -13,24 +13,27 @@ const Weather = z.object({
   })
 })
 
-export type Weather = z.infer<typeof Weather>
-
-export default function useWeather() {
-
-  const [weather, setWeather] = useState<Weather>({
-    name: '',
+const initialState = {
+  name: '',
     main: {
       temp: 0,
       temp_max: 0,
       temp_min: 0
     }
-  })
+}
+
+export type Weather = z.infer<typeof Weather>
+
+export default function useWeather() {
+
+  const [weather, setWeather] = useState<Weather>(initialState)
 
   const [loading, setLoading] = useState(false)
 
   const fetchWeather = async (search: SearchType) => {
 
     setLoading(true)
+    setWeather(initialState)
     
     try {
 
