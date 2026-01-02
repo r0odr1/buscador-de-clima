153e02e25fdffd8df7018f5e162f323b3a26b824
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-02 10:19:49 -0500

    imagen de fondo y css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-
+import styles from './App.module.css'
 
 function App() {
 
   return (
     <>
-      <h1>Buscador de Clima</h1>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
     </>
   )
 }
