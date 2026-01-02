990d4f16494886a161ac44879ac9d933689f3c9a
rodrigo.bonilla <rodrigo.bonilla@easysoft.com.ec>
2026-01-02 12:02:30 -0500

    creando hook para consultar el clima

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,7 +4,11 @@ import styles from './Form.module.css'
 import type { SearchType } from "../types";
 import Alert from "../Alert/Alert";
 
-export default function Form() {
+type FormProps = {
+  fetchWeather: () => void
+}
+
+export default function Form({fetchWeather} : FormProps) {
   const [search, setSearch] = useState<SearchType>({
     city: '',
     country: ''
@@ -26,7 +30,8 @@ export default function Form() {
       setAlert('Todos los campos son obligatorios')
       return
     }
-
+    setAlert('')
+    fetchWeather()
   }
 
   return (
---
+++ src/hooks/useWeather.ts
@@ -0,0 +1,11 @@
+
+export default function useWeather() {
+  const fetchWeather = () => {
+    console.log("Consultando...");
+    
+  }
+
+  return {
+    fetchWeather
+  }
+}
